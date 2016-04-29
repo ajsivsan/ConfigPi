071d3015ef9193f792e40898100bc37c5958c4de
Sivasankar Jayapal <ajsivsan@gmail.com>
2016-04-28 22:46:34 -0500

    Create repo

---
+++ src/main/java/com/ajsivsan/configpi/Controller/LightController.java
@@ -0,0 +1,24 @@
+package com.ajsivsan.configpi.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;;
+public class LightController {
+	private static GpioPinDigitalOutput pin;
+	@RequestMapping("/")
+	public String home(){
+		return "Welcomd to ConfigPi";
+	}
+	public String light(){
+		if(pin == null){
+			GpioController gpio = GpioFactory.getInstance();
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Light", PinState.LOW);
+		}
+		pin.toggle();
+		return "Success changing light state";
+	}
+}
