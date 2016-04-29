4ce3f674b7ceb46247ea544dae68a32a162b1cab
Sivasankar Jayapal <ajsivsan@gmail.com>
2016-04-28 22:53:28 -0500

    Added RestController

--- src/main/java/com/ajsivsan/configpi/Controller/LightController.java
+++ src/main/java/com/ajsivsan/configpi/Controller/LightController.java
@@ -1,12 +1,15 @@
 package com.ajsivsan.configpi.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.RaspiPin;;
+import com.pi4j.io.gpio.RaspiPin;
+
+@RestController
 public class LightController {
 	private static GpioPinDigitalOutput pin;
 	@RequestMapping("/")
